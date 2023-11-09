50d1ea14a0a21bedb91aefcabc095aff03e253b2
Mandeep Sharma <mandeepsharma1908@gmail.com>
2023-11-08 19:57:06 -0800

    VideoList page mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import "./App.scss";
 import WarehouseList from "./pages/WarehouseList/WarehouseList";
 import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails";
@@ -6,13 +6,30 @@ import WarehouseEdit from "./pages/WarehouseEdit/WarehouseEdit";
 import InventoryList from "./pages/InventoryList/InventoryList";
 import InventoryDetail from "./pages/InventoryDetail/InventoryDetail";
 import InventoryEdit from "./pages/InventoryEdit/InventoryEdit";
+import warehousesJSON from "./warehous.json";
+import { useState } from "react";
+// const baseURL = "http://localhost:5050";
 
 function App() {
+  const [warehouseDetails, setWarehouseDetails] = useState(warehousesJSON);
+  const [warehouseList, setWarehouseList] = useState(warehousesJSON);
+
+  function selectWarehouse(id) {
+    const selectedWarehouse = warehouseDetails.find(
+      (warehouse) => warehouse.id === id
+    );
+
+    setWarehouseList(selectedWarehouse);
+  }
+
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<WarehouseList />} />
+          <Route
+            path="/"
+            element={<WarehouseList warehouseList={warehouseList} />}
+          />
           <Route path="/details" element={<WarehouseDetails />} />
           <Route path="/edit" element={<WarehouseEdit />} />
           <Route path="/inventory/list" element={<InventoryList />} />
--- src/pages/WarehouseList/WarehouseList.jsx
+++ src/pages/WarehouseList/WarehouseList.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import "./WarehouseList.scss";
+import searchIcon from "../../assets/icons/search-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import forwardIcon from "../../assets/icons/chevron_right-24px.svg";
 
 function WarehouseList(props) {
-  return <div>WarehouseList</div>;
+  console.log("props:", props);
+  return (
+    <>
+      <div className="warehouse">
+        <h1 className="warehouse__header">Warehouses</h1>
+        <div className="searchbar-container">
+          <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            required
+          />
+          <img src={searchIcon} className="search-icon" alt="search icon" />
+        </div>
+        <button className="warehouse__add-button">+Add New Warehouse</button>
+      </div>
+      <ul className="warehouse__list">
+        {props.warehouseList.map((warehouse) => {
+          return (
+            <li key={warehouse.id} className="warehouse__list-item">
+              <div className="parent-container">
+                <div className="flexbox1">
+                  <div className="warehouse__description-container">
+                    <span className="warehouse__label">Warehouse</span>
+                    <p className="warehouse__city">
+                      {warehouse.city}
+                      <img src={forwardIcon} alt="link icon" />
+                    </p>
+                  </div>
+                  <div className="warehouse__description-container">
+                    <span className="warehouse__label">Address</span>
+                    <p>{warehouse.address}</p>
+                  </div>
+                </div>
+                <div className="flexbox2">
+                  <div className="warehouse__description-container">
+                    <span className="warehouse__label">Contact Name</span>
+                    <p className="warehouse__contact-item">
+                      {warehouse.contact_name}
+                    </p>
+                  </div>
+                  <div className="warehouse__description-container">
+                    <span className="warehouse__label">
+                      Contact Information
+                    </span>
+                    <p className="warehouse__info">{warehouse.contact_phone}</p>
+                    <p className="warehouse__info">{warehouse.contact_email}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="icon-container">
+                <img className="icons" src={deleteIcon} alt="delete icon" />
+                <img className="icons" src={editIcon} alt="edit icon" />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default WarehouseList;
